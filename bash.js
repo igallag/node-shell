afb502de968281ab1b25dc81228d71f9792012d4
Ian Gallgher <igallag93@gmail.com>
2019-08-05 12:51:34 -0400

    added cat

--- bash.js
+++ bash.js
@@ -2,21 +2,33 @@ process.stdout.write('prompt > ');
 
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 const fs = require('fs');
 
-
+//console.log(cat)
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
 
+    const cmdArr = cmd.split(' ');
+
+    const command = cmdArr[0];
+    const fileName = cmdArr[1];
+
+
+
 
     //process.stdout.write('You typed: ' + cmd);
-    if (cmd === 'pwd') {
+    if (command === 'pwd') {
         pwd();
     }
 
-    if (cmd === 'ls') {
+    if (command === 'ls') {
         ls();
     }
+
+    if (command === 'cat') {
+        cat(fileName);
+    }    
 
     //process.stdout.write('\nprompt > ');
 })
